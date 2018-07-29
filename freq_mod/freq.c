c5609f164f1e1d530e23af09ab895946b484bec2
Michael Johns <michaelwaynejohns@gmail.com>
2018-07-29 10:24:39 -0500

    Frequency measurement device driver

---
+++ freq_mod/freq.c
@@ -0,0 +1,145 @@
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/time.h>
+#include <linux/uaccess.h>
+
+#define DEV_NAME "freq"
+#define DEFAULT_GPIO_PIN 17
+#define BUFFER_SIZE 16
+
+static struct timespec prev_time;
+static unsigned long ns_periods[BUFFER_SIZE];
+static int read_ptr;
+static int write_ptr;
+static int gpio_pin = DEFAULT_GPIO_PIN;
+static int freq_irq = -1;
+
+/* ISR called on every GPIO signal edge */
+static irqreturn_t freq_isr(int irq, void *data)
+{
+	struct timespec cur_time;
+	struct timespec delta;
+	unsigned long ns;
+
+	getnstimeofday(&cur_time);
+	delta = timespec_sub(cur_time, prev_time);
+	ns = ((long long)delta.tv_sec * 1000000000) + delta.tv_nsec;
+	ns_periods[write_ptr] = ns;
+	prev_time = cur_time;
+
+	write_ptr = (write_ptr + 1) % BUFFER_SIZE;
+	if (write_ptr == read_ptr) {
+		read_ptr = (read_ptr + 1) % BUFFER_SIZE;
+	}
+
+	return IRQ_HANDLED;
+}
+
+static int freq_open(struct inode *inode, struct file *file)
+{
+	return nonseekable_open(inode, file);
+}
+
+static int freq_release(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static ssize_t freq_write(struct file *file, const char __user *buf, size_t count, loff_t *pos)
+{
+	return -EINVAL;
+}
+
+static ssize_t freq_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
+{
+	char str[16];
+	int error, char_count = 0;
+
+	if (read_ptr != write_ptr) {
+		sprintf(str, "%ld\n", ns_periods[read_ptr]);
+		char_count = strlen(str);
+		error = copy_to_user(buf, str, char_count + 1);
+		if (error != 0) {
+			printk(KERN_ERR "%s: copy_to_user returned %d\n", __func__, error);
+			return -EFAULT;
+		}
+		read_ptr = (read_ptr + 1) % BUFFER_SIZE;
+	}
+	return char_count;
+}
+
+static struct file_operations freq_fops = {
+	.owner = THIS_MODULE,
+	.open = freq_open,
+	.read = freq_read,
+	.write = freq_write,
+	.release = freq_release,
+};
+
+static struct miscdevice freq_misc_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = DEV_NAME,
+	.fops = &freq_fops,
+};
+
+static int __init freq_init(void){
+	int ret = 0;
+	printk(KERN_INFO "%s: start\n", __func__);
+
+	getnstimeofday(&prev_time);
+	write_ptr = 0;
+	read_ptr = 0;
+
+	ret = gpio_request(gpio_pin, "input");
+	if (ret) {
+		printk(KERN_ERR "%s: gpio_request(%d, \"input\") returned %d\n", __func__, gpio_pin, ret);
+		goto fail;
+	}
+	ret = gpio_direction_input(gpio_pin);
+	if (ret) {
+		printk(KERN_ERR "%s: gpio_direction_input(%d) returned %d\n", __func__, gpio_pin, ret);
+		goto fail;
+	}
+	ret = gpio_to_irq(gpio_pin);
+	if (ret < 0) {
+		printk(KERN_ERR "%s: gpio_to_irq(%d) returned %d\n", __func__, gpio_pin, ret);
+		goto fail;
+	}
+	freq_irq = ret;
+	ret = request_irq(freq_irq, freq_isr, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "freq", NULL);
+	if (ret) {
+		printk(KERN_ERR "%s: request_irq returned %d\n", __func__, ret);
+		free_irq(freq_irq, NULL);
+		goto fail;
+	}
+
+	misc_register(&freq_misc_device);
+
+	printk(KERN_INFO "%s: end\n", __func__);
+	return ret;
+
+fail:
+	gpio_free(gpio_pin);
+	return ret;
+}
+
+static void __exit freq_exit(void){
+	printk(KERN_INFO "%s: start\n", __func__);
+
+	misc_deregister(&freq_misc_device);
+	free_irq(freq_irq, NULL);
+	gpio_free(gpio_pin);
+
+	printk(KERN_INFO "%s: end\n", __func__);
+}
+
+module_init(freq_init);
+module_exit(freq_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Michael Johns <michaelwaynejohns@gmail.com>");
+MODULE_DESCRIPTION("Frequency measurement device driver");
