6e3c4a66b2ca78e1aba398c575f5ad5e4400cf2c
Michael Johns <michaelwaynejohns@gmail.com>
2020-07-31 21:20:51 -0500

    Thermostat webui * host jquery locally so that it works over https * update everything through js instead of waiting for server side completion and refresh

--- thermostat/html/thermostat.php
+++ thermostat/html/thermostat.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 
 <html>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src='jquery-3.5.1.min.js'></script>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
-        <script type="text/javascript">
+        <meta http-equiv='Content-Type' content='text/html; charset=utf8' />
+        <script type='text/javascript'>
 
             function refresh() {
                 var elems = document.getElementsByClassName('updateme');
                 for (var i = 0; i < elems.length; i++) {
                     let req = new XMLHttpRequest();
                     let elem = elems[i];
-                    let prefix = elem.getAttribute('prefix');
                     let txtfile = 'thermostat_files/' + elem.getAttribute('txtfile');
                     req.onreadystatechange = function () {
                         if (req.readyState == 4 && req.status == 200) {
-                            elem.innerText = prefix + req.responseText;
+                            elem.innerText = req.responseText;
                         }
                     }
                     req.open('GET', txtfile, true);
@@ -28,10 +27,15 @@
                 refresh();
                 var int = self.setInterval(function () {
                     refresh();
-                }, 1000);
+                }, 5000);
             }
 
             function button(elem) {
+                if (elem.hasAttribute('settext')) {
+                    var settext_name = elem.getAttribute('settext');
+                    var settext_elem = document.getElementById(settext_name);
+                    settext_elem.innerText = elem.id;
+                }
                 if (elem.hasAttribute('uncolor')) {
                     var uncolor_names = elem.getAttribute('uncolor');
                     var uncolor_array = uncolor_names.split(',');
@@ -43,6 +47,20 @@
                 if (elem.hasAttribute('colorme')) {
                     var new_color = elem.getAttribute('colorme');
                     elem.style.backgroundColor = new_color;
+                }
+                if (elem.hasAttribute('decrement')) {
+                    var decrement_name = elem.getAttribute('decrement');
+                    var decrement_elem = document.getElementById(decrement_name);
+                    var number = decrement_elem.innerText;
+                    number--;
+                    decrement_elem.innerText = number;
+                }
+                if (elem.hasAttribute('increment')) {
+                    var increment_name = elem.getAttribute('increment');
+                    var increment_elem = document.getElementById(increment_name);
+                    var number = increment_elem.innerText;
+                    number++;
+                    increment_elem.innerText = number;
                 }
                 var python_args = elem.className + ' ' + elem.id;
                 $.ajax({
@@ -61,35 +79,28 @@
 
 <h2>Thermostat</h2>
 
-<h3>
-    <a class='updateme' txtfile='curstat.txt' prefix='Status: '></a>
-    <a class='updateme' txtfile='averagetemp.txt' prefix=' '></a>
-</h3>
-<h4 class='updateme' txtfile='bedroomtemp.txt' prefix='Bedroom: '></h4>
-<h4 class='updateme' txtfile='nurserytemp.txt' prefix='Nursery: '></h4>
-<h4 class='updateme' txtfile='familyroomtemp.txt' prefix='Family room: '></h4>
-<h4 class='updateme' txtfile='basementtemp.txt' prefix='Basement: '></h4>
-<h4 class='updateme' txtfile='closettemp.txt' prefix='Closet: '></h4>
+<h3>Status: <a class='updateme' txtfile='curstat.txt'></a> <a class='updateme' txtfile='averagetemp.txt'></a></h3>
+<h4>Bedroom: <a class='updateme' txtfile='bedroomtemp.txt'></a></h4>
+<h4>Nursery: <a class='updateme' txtfile='nurserytemp.txt'></a></h4>
+<h4>Family room: <a class='updateme' txtfile='familyroomtemp.txt'></a></h4>
+<h4>Basement: <a class='updateme' txtfile='basementtemp.txt'></a></h4>
+<h4>Closet: <a class='updateme' txtfile='closettemp.txt'></a></h4>
 
 <h3>Settings</h3>
-<h4 class='updateme' txtfile='temp.txt' prefix='Temperature: '></h4>
-<button class='temp' id='Down' onclick='button(this)' style='background-color:silver'>Down</button>
-<button class='temp' id='Up' onclick='button(this)' style='background-color:silver'>Up</button>
-<h4 class='updateme' txtfile='mode.txt' prefix='Mode: '></h4>
-<button class='mode' id='Cool' onclick='button(this)' colorme='aqua' uncolor='Heat' style='background-color:<?php include('thermostat_files/cool_color.txt'); ?>'>Cool</button>
-<button class='mode' id='Heat' onclick='button(this)' colorme='orange' uncolor='Cool' style='background-color:<?php include('thermostat_files/heat_color.txt'); ?>'>Heat</button>
-<button class='mode' id='Off' onclick='button(this)' uncolor='Heat,Cool' style='background-color:silver'>Off</button>
-<h4 class='updateme' txtfile='fan.txt' prefix='Fan: '></h4>
-<button class='fan' id='Auto' onclick='button(this)' colorme='lime' uncolor='On' style='background-color:<?php include('thermostat_files/auto_color.txt'); ?>'>Auto</button>
-<button class='fan' id='On' onclick='button(this)' colorme='lime' uncolor='Auto' style='background-color:<?php include('thermostat_files/on_color.txt'); ?>'>On</button>
+<h4>Temperature: <a class='updateme' id='temp' txtfile='temp.txt'></a></h4>
+<button class='temp' id='Down' onclick='button(this)' decrement='temp' style='background-color:silver'>Down</button>
+<button class='temp' id='Up' onclick='button(this)' increment='temp' style='background-color:silver'>Up</button>
+<h4>Mode: <a class='updateme' id='mode' txtfile='mode.txt'></a></h4>
+<button class='mode' id='Cool' onclick='button(this)' settext='mode' colorme='aqua' uncolor='Heat' style='background-color:<?php include('thermostat_files/cool_color.txt'); ?>'>Cool</button>
+<button class='mode' id='Heat' onclick='button(this)' settext='mode' colorme='orange' uncolor='Cool' style='background-color:<?php include('thermostat_files/heat_color.txt'); ?>'>Heat</button>
+<button class='mode' id='Off' onclick='button(this)' settext='mode' uncolor='Heat,Cool' style='background-color:silver'>Off</button>
+<h4>Fan: <a class='updateme' id='fan' txtfile='fan.txt'></a></h4>
+<button class='fan' id='Auto' onclick='button(this)' settext='fan' colorme='lime' uncolor='On' style='background-color:<?php include('thermostat_files/auto_color.txt'); ?>'>Auto</button>
+<button class='fan' id='On' onclick='button(this)' settext='fan' colorme='lime' uncolor='Auto' style='background-color:<?php include('thermostat_files/on_color.txt'); ?>'>On</button>
 
 <h3>Statistics</h3>
-<h4>
-    <a class='updateme' txtfile='hourdutycycle.txt' prefix='Duty Cycle: '></a>
-    <a class='updateme' txtfile='daydutycycle.txt' prefix=' (hour) '></a>
-    (day)
-</h4>
-<h4 class='updateme' txtfile='lastupdate.txt' prefix='Last Update: '></h4>
+<h4>Duty Cycle: <a class='updateme' txtfile='hourdutycycle.txt'></a> (hour) <a class='updateme' txtfile='daydutycycle.txt'></a> (day)</h4>
+<h4>Last Update: <a class='updateme' txtfile='lastupdate.txt'></a></h4>
 
 </body>
 </html>
